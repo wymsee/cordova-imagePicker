018995d040ac58417d8ed6d02d5a29d41594cf81
leonzhang1109 <leonzhang1109@hotmail.com>
2015-02-11 19:31:55 -0800

    Update MultiImageChooserActivity.java

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -617,7 +617,7 @@ public class MultiImageChooserActivity extends Activity implements OnItemClickLi
         */
         private File storeImage(Bitmap bmp, String fileName) throws IOException {
             int index = fileName.lastIndexOf('.');
-            String name = fileName.substring(0, index);
+            String name = "Temp_" + fileName.substring(0, index);
             String ext = fileName.substring(index);
             File file = File.createTempFile(name, ext);
             OutputStream outStream = new FileOutputStream(file);
