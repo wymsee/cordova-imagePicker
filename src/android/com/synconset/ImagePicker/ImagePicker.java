f524bcceaba60ddc8a8d19dcac11cd07d71c0a86
Simon Jarbrant <simonjarbrant@gmail.com>
2016-01-07 15:45:35 +0100

    Removed unused import from ImagePicker

--- src/android/com/synconset/ImagePicker/ImagePicker.java
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -6,7 +6,6 @@ package com.synconset;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
